a029c19ff9e47be807f5007f3d85dff19ca7c2d4
gponciano <gab_bash@hotmail.com>
2025-08-26 21:25:21 +1000

    adding database module

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from database import db
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
@@ -7,8 +7,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
 
 # Initiating instance for the class, SQLAlchemy is the class whereas app is the instance
-db = SQLAlchemy(app)
-
+db.init_app(app)
 
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
@@ -17,5 +16,5 @@ def hello_world():
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-
+# db connect: flask shell
+# sqlite viewer extension is needed
