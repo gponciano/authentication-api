1283c517477fdb70e903cf67ea146ebdfa22eaf6
gponciano <gab_bash@hotmail.com>
2025-08-26 21:08:46 +1000

    added UserMixin to User

--- models/user.py
+++ models/user.py
@@ -1,9 +1,10 @@
 from app import db
+from flask_login import UserMixin
 
-class User(db.Model):
+# Multiple inheritance for User model
+class User(db.Model, UserMixin):
     # id(int):PK, username(string), password(text)
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
 
-    
